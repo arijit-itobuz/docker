a6fca4c506984fc7111b7e7056ce58c96b5264b0
Arijit Das <arijit@itobuz.com>
2024-01-03 15:19:05 +0530

    feat: run.sh added

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo $0
+
+echo "Hello World $0 $1 $2"
+date
+pwd
+ls
+which bash
+my_env=ARIJIT
+echo $my_env
+
+echo "Enter your name"
+read YOUR_NAME
+echo "My name is" $YOUR_NAME
